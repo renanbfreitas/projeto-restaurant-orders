135bdcde40b31c47d639e2f7621fe6cb12b71661
Renanbfreitas <renanbf1992@outlook.com>
2023-06-19 12:35:27 -0300

    feat: add req de testes

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,28 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction # noqa: F401, E261, E501
+import pytest
 
 
-# Req 2
 def test_dish():
-    pass
+    dish_one = Dish('food', 3)
+    assert dish_one.name == 'food'
+    dish_two = Dish('food', 3)
+    assert repr(dish_one) == "Dish('food', R$3.00)"
+    assert repr(dish_one) != "Dish('food', R$2.00)"
+    assert hash(dish_one) == hash(Dish('food', 3))
+    assert hash(dish_one) != hash(Dish('food', 1))
+    assert dish_one != Dish('food', 1)
+    assert dish_one == Dish('food', 3)
+    with pytest.raises(TypeError):
+        Dish(3, 'food')
+    with pytest.raises(ValueError):
+        Dish('food', -3)
+
+    ingrediente_frango = Ingredient('frango')
+    ingrediente_creme_de_leite = Ingredient('creme de leite')
+    dish_one.add_ingredient_dependency(ingrediente_frango, 1)
+    dish_two.add_ingredient_dependency(ingrediente_frango, 3)
+    assert dish_one.get_ingredients() == {Ingredient('frango')}
+    assert dish_one.get_restrictions() == dish_two.get_restrictions()
+    dish_two.add_ingredient_dependency(ingrediente_creme_de_leite, 3)
+    assert dish_one.get_restrictions() != dish_two.get_restrictions()
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,31 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingrediente_frango = Ingredient('frango')
+
+    assert ingrediente_frango.name == 'frango'
+    assert ingrediente_frango.name != 'creme de leite'
+    assert Restriction.ANIMAL_MEAT in ingrediente_frango.restrictions
+    assert Restriction.ANIMAL_DERIVED in ingrediente_frango.restrictions
+    assert Restriction.GLUTEN not in ingrediente_frango.restrictions
+    assert Restriction.SEAFOOD not in ingrediente_frango.restrictions
+    assert ingrediente_frango.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert ingrediente_frango.restrictions != {
+        Restriction.GLUTEN,
+        Restriction.SEAFOOD,
+    }
+
+    assert hash(ingrediente_frango) == hash('frango')
+    assert hash(ingrediente_frango) != hash('creme de leite')
+    assert repr(ingrediente_frango) == "Ingredient('frango')"
+    assert repr(ingrediente_frango) != "Ingredient('creme de leite')"
+    assert ingrediente_frango == Ingredient('frango')
+    assert ingrediente_frango != Ingredient('creme de leite')
